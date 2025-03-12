0982d0d2b57157cd1d07b737a3055fafdad05ac1
Gleb <gleb.moiseev@tgadigital.com>
2025-03-12 15:45:11 +0200

    fix deployment issue

--- src/app/ui/fonts.ts
+++ src/app/ui/fonts.ts
@@ -4,9 +4,11 @@ const inter = Inter({ subsets: ['latin'] });
 
 const lusitanaNormal = Lusitana({
   weight: '400',
+  subsets: ['latin'],
 });
 const lusitanaBold = Lusitana({
   weight: '700',
+  subsets: ['latin'],
 });
 
 export { inter, lusitanaNormal, lusitanaBold };
